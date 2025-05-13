242ed95bfca6884ed58312b71e1a26dcddf29ea8
NatiF <65537815+OptimaLPro@users.noreply.github.com>
2025-05-13 13:38:45 +0300

    feat: add new components and features for enhanced user experience
    - Added embla-carousel-react for carousel functionality.
    - Introduced a new HomeCarousel component for displaying images in a carousel format.
    - Updated Navbar to include a new analysis link and reorganized icon imports.
    - Enhanced Home page layout with RTL support and added score display.
    - Created a new home-chart component for visualizing data with recharts.
    - Implemented a reusable Card component for consistent UI design.
    - Developed a ChartContainer for better chart management and styling.
    - Added a Carousel component with navigation buttons for improved usability.
    - Updated App.css to support RTL styling.

---
+++ src/pages/Home/Home-Carousel.jsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export function HomeCarousel() {
+  return (
+    <Carousel className="w-full max-w-xs">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1 aspect-square overflow-hidden rounded-xl">
+              <img
+                src={`https://picsum.photos/seed/picsum${index}/300/300`}
+                alt={`Random ${index}`}
+                className="object-cover w-full h-full rounded-xl"
+              />
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { Component } from "@/components/charts/home-chart";
+import React from "react";
+import { HomeCarousel } from "./Home-Carousel";
 
 const Home = () => {
   return (
-    <div className=''>
-
+    <div className="min-h-screen flex flex-col mx-auto items-center mb-10">
+      {/* Score */}
+      <div className="rtl text-center flex flex-col leading-15 mt-10">
+        <span className="text-[40px] mb-6">שלום נתי!</span>
+        <span className="text-[40px] mb-6">ברשותך:</span>
+        <span className="text-[90px]">235</span>
+        <span className="text-[50px] mt-1">נקודות</span>
+      </div>
+      {/* Goal */}
+      <div className="rtl text-center flex flex-col leading-15 mt-10">
+        {/* <span className="text-[40px]">יעד</span>
+        <span className="text-[30px] mt-1">500 נקודות</span> */}
+        <Component />
+      </div>
+      <div className=" text-center flex flex-col leading-15 mt-10">
+        <span className="text-[40px] mt-10 rtl text-right">🔥הטבות</span>
+        <HomeCarousel />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
